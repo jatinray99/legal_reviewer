a3dfdcf10226cd0f5a0093a593262697ef737754
Jatin Ray <97835550+jatinray99@users.noreply.github.com>
2025-11-20 10:57:32 +0530

    Add files via upload

---
+++ root/AuthContext.tsx
@@ -0,0 +1,37 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { auth } from "../firebase";
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+
+const AuthContext = createContext<any>(null);
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const loginWithGoogle = async () => {
+    const provider = new GoogleAuthProvider();
+    await signInWithPopup(auth, provider);
+  };
+
+  const logout = () => signOut(auth);
+
+  useEffect(() => {
+    return onAuthStateChanged(auth, (u) => {
+      setUser(u);
+      setLoading(false);
+    });
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, loginWithGoogle, logout }}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
